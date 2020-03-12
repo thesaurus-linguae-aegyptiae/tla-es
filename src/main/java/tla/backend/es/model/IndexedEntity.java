259a3190807f98e09bbe8651cb4d47f1bbe98300
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2020-03-12 10:45:38 +0100

    use abstract model base class from library

--- src/main/java/tla/backend/es/model/IndexedEntity.java
+++ src/main/java/tla/backend/es/model/IndexedEntity.java
@@ -11,19 +11,21 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 
-import tla.domain.model.meta.BTSeClass;
+import tla.domain.model.meta.AbstractBTSBaseClass;
 
 @Data
 @Slf4j
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class IndexedEntity {
+@EqualsAndHashCode(callSuper = true)
+public abstract class IndexedEntity extends AbstractBTSBaseClass {
 
     @Id
     @NonNull
@@ -44,22 +46,6 @@ public abstract class IndexedEntity {
             );
             return entity.id;
         }
-    }
-
-    /**
-     * Returns the object's <code>eClass</code> value specified via the {@link BTSeClass} annotation.
-     */
-    public String getEclass() {
-        for (Annotation annotation : this.getClass().getAnnotations()) {
-            if (annotation instanceof BTSeClass) {
-                return ((BTSeClass) annotation).value();
-            }
-        }
-        log.warn(
-            "eClass of {} instance not specified via @BTSeClass annotation. Returning class name",
-            this.getClass().getName()
-        );
-        return this.getClass().getName();
     }
 
 }
