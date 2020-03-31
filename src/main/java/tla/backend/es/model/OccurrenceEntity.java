83cba465d327246386bf4bde268cca0abd2ef308
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-03-31 23:12:32 +0200

    fix model issues

--- src/main/java/tla/backend/es/model/OccurrenceEntity.java
+++ src/main/java/tla/backend/es/model/OccurrenceEntity.java
@@ -10,11 +10,14 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -58,7 +61,7 @@ public class OccurrenceEntity implements Indexable {
     @Builder
     public static class Lemmatization {
 
-        private Integer pos;
+        private String pos;
 
         @Field(type = FieldType.Keyword)
         private String id;
