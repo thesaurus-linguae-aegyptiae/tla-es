636071b3108ffc4b5cd3158f5cf47f70def45995
dwerning <33833393+dwerning@users.noreply.github.com>
2022-04-01 18:10:38 +0200

    search terms in translation understood as AND

--- src/main/java/tla/backend/es/query/MultiLingQueryBuilder.java
+++ src/main/java/tla/backend/es/query/MultiLingQueryBuilder.java
@@ -3,6 +3,7 @@ package tla.backend.es.query;
 import static org.elasticsearch.index.query.QueryBuilders.*;
 
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
 
 import tla.domain.command.TranslationSpec;
 import tla.domain.model.Language;
@@ -14,14 +15,15 @@ public interface MultiLingQueryBuilder extends TLAQueryBuilder {
         if (translation != null && translation.getLang() != null) {
             if (translation.getText() != null && !translation.getText().isBlank()) {
                 for (Language lang : translation.getLang()) {
-                    translationsQuery.should(
+                    this.must(
                         matchQuery(
                             String.format("%stranslations.%s", this.nestedPath(), lang),
                             translation.getText()
-                        )
+                        ).operator(Operator.AND)
                     );
                 }
             }
+        }
             /*var termSpecified = translation.getText() != null && !translation.getText().isBlank();
             for (Language lang : translation.getLang()) {
                 translationsQuery.should(
@@ -34,9 +36,9 @@ public interface MultiLingQueryBuilder extends TLAQueryBuilder {
                         String.format("%stranslations.%s", this.nestedPath(), lang)
                     )
                 );
-            }*/
+            }
         }
-        this.filter(translationsQuery);
+        this.filter(translationsQuery);*/
     }
 
 }
